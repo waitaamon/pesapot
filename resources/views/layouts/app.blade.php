c8e289fb6ba9b9e511c8bf3895ecf93e97d2b47e
Amon Waita <waitaamon@gmail.com>
2021-04-27 17:30:19 +0300

    added logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,11 +18,11 @@
     </head>
     <body class="font-sans antialiased">
     <div id="app">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-blue-50">
         @include('layouts.navigation')
 
         <!-- Page Heading -->
-            <header class="bg-white shadow">
+            <header class="bg-blue-100 shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-white border-b border-gray-100">
+<nav class="bg-blue-100 border-b border-white">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
